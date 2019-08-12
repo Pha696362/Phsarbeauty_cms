a90eab3df1efeba60fa8328a12b5553525bb3f4c
khemarachhe <khemarachhe@gmail.com>
2019-08-12 16:14:37 +0700

    f

--- src/app/stores/app.store.ts
+++ src/app/stores/app.store.ts
@@ -2,10 +2,12 @@ import { Bookstore } from './bookstore';
 import { Environment } from './environment.store';
 import { Search } from './search.store';
 import { AuthStore } from './auth.store';
+import { BaseStore } from './base.store';
 
 export const APP_STORES = [
   Environment,
   Search,
   AuthStore,
-  Bookstore
+  Bookstore,
+  BaseStore,
 ];
