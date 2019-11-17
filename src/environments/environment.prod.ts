26922f900c2784937e37001c632b99906699265e
KhemPha <khempha@KhemPhas-MacBook-Pro.local>
2019-11-17 11:56:09 +0700

    Mr.Pha

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,14 +2,24 @@ export const environment = {
   production: true,
   firebase: {
    
-    apiKey: "AIzaSyBxZxeec1QA-vQtrBuKcr0nKgZz0tkU3Ok",
-    authDomain: "thecambodiareviews-d2ccc.firebaseapp.com",
-    databaseURL: "https://thecambodiareviews-d2ccc.firebaseio.com",
-    projectId: "thecambodiareviews-d2ccc",
-    storageBucket: "thecambodiareviews-d2ccc.appspot.com",
-    messagingSenderId: "908629667416",
-    appId: "1:908629667416:web:594905db57e283828e642d",
-    measurementId: "G-3F4W30TFRN"
+    // apiKey: "AIzaSyBxZxeec1QA-vQtrBuKcr0nKgZz0tkU3Ok",
+    // authDomain: "thecambodiareviews-d2ccc.firebaseapp.com",
+    // databaseURL: "https://thecambodiareviews-d2ccc.firebaseio.com",
+    // projectId: "thecambodiareviews-d2ccc",
+    // storageBucket: "thecambodiareviews-d2ccc.appspot.com",
+    // messagingSenderId: "908629667416",
+    // appId: "1:908629667416:web:594905db57e283828e642d",
+    // measurementId: "G-3F4W30TFRN"
+    apiKey: "AIzaSyBO2frs-_XP3wppZnUyamdLdwnYH9E0_NY",
+    authDomain: "phsarbeauty-d5128.firebaseapp.com",
+    databaseURL: "https://phsarbeauty-d5128.firebaseio.com",
+    projectId: "phsarbeauty-d5128",
+    storageBucket: "phsarbeauty-d5128.appspot.com",
+    messagingSenderId: "404553505586",
+    appId: "1:404553505586:web:b8e1af65b503690815552b",
+    measurementId: "G-69MNENTM2Z"
+
+
   },
   mapKey: 'AIzaSyAeIMIEUOK0t0HCYoCvWXfeS-kf3HJBJa8'
 };
